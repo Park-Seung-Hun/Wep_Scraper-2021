b0c5b976cde7f42269bfaaf7865d5bd1d3481705
ParkSH <psh19930326@gmail.com>
2021-02-24 16:19:09 +0900

    feat: extracting titles

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
+
+last_indeed_pages = extract_indeed_pages()
+
+extract_indeed_jobs(last_indeed_pages)
 
 
-indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50")
-
-indeed_soup = BeautifulSoup(indeed_result.text,"html.parser") # html 분석을 위한 BeautifulSoup
-
-pagination= indeed_soup.find("ul",{"class":"pagination-list"}) # class명이 pagination을 찾기 위한 변수
-
-links = pagination.find_all('a') # 모든 a 태그를 찾기 위한 변수
-
-pages=[]
-for link in links[:-1]: # next를 제와한 페이지 숫자를 가져온다.
-    pages.append(int(link.string)) # text를 추출하여 숫자로 바꾸어 저장한다.
-
-max_page = pages[-1]
