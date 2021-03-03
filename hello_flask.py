408c91dc3d5736b6de3ce3bc568f35497ef6344b
ParkSH <psh19930326@gmail.com>
2021-03-03 15:27:47 +0900

    complete webScrapper

--- hello_flask.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-
-app = Flask("HelloScrapper")
-
-@app.route("/") # "/"에 접속하면 home이라는 함수 실행 -> /는 root이다.
-def home():
-    return render_template("potato.html")
-
-app.run(host="127.0.0.1")
